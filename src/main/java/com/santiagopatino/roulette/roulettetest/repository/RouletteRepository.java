891ab2ef6b302d74e352e14d8cb0acd10771cd3f
santiago-pat-mon <32517162+santiago-pat-mon@users.noreply.github.com>
2021-02-07 12:00:33 -0500

    Update RouletteRepository.java
    Delete spaces in the code in RouletteRepository.java

--- src/main/java/com/santiagopatino/roulette/roulettetest/repository/RouletteRepository.java
+++ src/main/java/com/santiagopatino/roulette/roulettetest/repository/RouletteRepository.java
@@ -14,40 +14,32 @@ import java.util.UUID;
 
 @Repository
 public class RouletteRepository implements RouletteRepositoryInterface {
-
     private static final String KEY="Roulette";
-
     private RedisTemplate<String, Roulette> redisTemplateRoulette;
     private HashOperations hashOperations;
-
     public RouletteRepository(RedisTemplate<String, Roulette> redisTemplateRoulette) {
         this.redisTemplateRoulette = redisTemplateRoulette;
     }
-
     @PostConstruct
     private void init() {
         hashOperations = redisTemplateRoulette.opsForHash();
     }
-
     @Override
     public Map<String, Roulette> findAllRoulette() {
         return hashOperations.entries(KEY);
     }
-
     @Override
     public String createRoulette(Roulette roulette) {
         String id = UUID.randomUUID().toString();
         hashOperations.put(KEY, id, roulette);
         return id;
     }
-
     @Override
     public void openRoulette(String id) {
         Roulette roulette=findRouletteById(id);
         roulette.setStatus(true);
         updateRoulette(id,roulette);
     }
-
     @Override
     public boolean createBet(String idRoulette, Bet bet) {
         Roulette roulette = findRouletteById(idRoulette);
@@ -58,7 +50,7 @@ public class RouletteRepository implements RouletteRepositoryInterface {
         {
             roulette.getBets().add(bet);
             updateRoulette(idRoulette,roulette);
-
+            
             return true;
         }
         else{
@@ -66,7 +58,6 @@ public class RouletteRepository implements RouletteRepositoryInterface {
             return false;
         }
     }
-
     @Override
     public ArrayList<Bet> closeRoulette(String id) {
         Roulette roulette=findRouletteById(id);
@@ -87,22 +78,20 @@ public class RouletteRepository implements RouletteRepositoryInterface {
             if (bet.getColor().equals(winnerColor)) {
                 giveValue += bet.getValue() * 1.8;
             }
-
             bet.setValue(giveValue);
             betResult.add(bet);
             giveValue = 0.0;
         }
         roulette.setStatus(false);
         updateRoulette(id,roulette);
+        
         return betResult;
     }
-
     public void updateRoulette (String id, Roulette roulette){
         hashOperations.put(KEY,id,roulette);
     }
-
     public Roulette findRouletteById (String id){
-
+        
         return (Roulette)hashOperations.get(KEY,id);
     }
 }
