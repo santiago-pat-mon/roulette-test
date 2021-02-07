1cfad4c945c3957b16995eed8c1e39381f3a29c4
santiago-pat-mon <32517162+santiago-pat-mon@users.noreply.github.com>
2021-02-07 11:58:18 -0500

    Update Roulette.java
    Delete space in the code in Roulette.java

--- src/main/java/com/santiagopatino/roulette/roulettetest/domain/Roulette.java
+++ src/main/java/com/santiagopatino/roulette/roulettetest/domain/Roulette.java
@@ -1,26 +1,19 @@
 package com.santiagopatino.roulette.roulettetest.domain;
-
 import com.santiagopatino.roulette.roulettetest.Bet;
-
 import java.io.Serializable;
 import java.util.ArrayList;
-
 public class Roulette implements Serializable {
     ArrayList<Bet> bets = new ArrayList<Bet>();
     private boolean status;
-
     public ArrayList<Bet> getBets() {
         return bets;
     }
-
     public void setBets(ArrayList<Bet> bets) {
         this.bets = bets;
     }
-
     public boolean isStatus() {
         return status;
     }
-
     public void setStatus(boolean status) {
         this.status = status;
     }
