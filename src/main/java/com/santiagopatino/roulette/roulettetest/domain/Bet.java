fba79893518a82d8a1fed34f4db804470d96be71
santiago-pat-mon <32517162+santiago-pat-mon@users.noreply.github.com>
2021-02-07 11:57:36 -0500

    Update Bet.java
    Delete space in the code in Bet.java

--- src/main/java/com/santiagopatino/roulette/roulettetest/domain/Bet.java
+++ src/main/java/com/santiagopatino/roulette/roulettetest/domain/Bet.java
@@ -1,41 +1,31 @@
 package com.santiagopatino.roulette.roulettetest;
-
 import java.io.Serializable;
-
 public class Bet implements Serializable {
     private Integer number;
     private Double value;
     private String color;
     private String user;
-
     public Integer getNumber() {
         return number;
     }
-
     public void setNumber(Integer number) {
         this.number = number;
     }
-
     public Double getValue() {
         return value;
     }
-
     public void setValue(Double value) {
         this.value = value;
     }
-
     public String getColor() {
         return color;
     }
-
     public void setColor(String color) {
         this.color = color;
     }
-
     public String getUser() {
         return user;
     }
-
     public void setUser(String user) {
         this.user = user;
     }
